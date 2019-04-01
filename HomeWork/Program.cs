e17283c60daf1b2f8a8098334fd191c82a687146
Anton <antonio.de.tores@gmail.com>
2019-04-01 21:09:54 +0300

    Task1 implemented

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -11,5 +11,11 @@ namespace HomeWork
         static void Main(string[] args)
         {
         }
+
+        private static void Task1()
+        {
+            int x1 = 0;
+            Console.WriteLine($"x1: {x1}");
+        }
     }
 }
