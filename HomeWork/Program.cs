627ab60381c954eddd2f69c0c7b6635dafabd445
Anton <antonio.de.tores@gmail.com>
2019-04-01 21:10:35 +0300

    Task2 implemented

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -17,5 +17,13 @@ namespace HomeWork
             int x1 = 0;
             Console.WriteLine($"x1: {x1}");
         }
+
+        private static void Task2()
+        {
+            Console.WriteLine("Ваше имя?");
+            string str1 = Console.ReadLine();
+            string str2 = "Привет " + str1;
+            Console.WriteLine(str2);
+        }
     }
 }
