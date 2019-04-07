11b4baf49d40abf0c61d1becaa3d4fd2fd9fa45d
Anton <antonio.de.tores@gmail.com>
2019-04-07 16:04:32 +0300

    Task1 implemented

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -11,5 +11,28 @@ namespace HomeWork
         static void Main(string[] args)
         {
         }
+
+        private static void Task1()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int factorial = 1;
+
+            if (number % 1 == 0 && number > 0)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine($"{number}! = {factorial}");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("0! = 1");
+            }
+            else
+            {
+                Console.WriteLine("Enter valid number");
+            }
+        }
     }
 }
