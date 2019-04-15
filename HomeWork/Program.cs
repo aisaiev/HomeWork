c9cf22c574dec1b5263e755b2658ff83e83acafc
Anton <antonio.de.tores@gmail.com>
2019-04-15 22:42:11 +0300

    Task1 implemented

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -8,8 +8,63 @@ namespace HomeWork
 {
     class Program
     {
+        private static Position position;
         static void Main(string[] args)
         {
         }
+
+        private static void Task1()
+        {
+            Console.Write("Enter size of array: ");
+            int arraySize = int.Parse(Console.ReadLine());
+            Console.Write("Enter elements of array through the space: ");
+            string arrayString = Console.ReadLine();
+            string[] stringArray = arrayString.Split(' ');
+            int[] intArray = new int[arraySize];
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                intArray[i] = int.Parse(stringArray[i]);
+            }
+            if (intArray[0] == 0)
+            {
+                position = Position.One;
+            }
+            else
+            {
+                position = Position.Zero;
+            }
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (position == Position.Zero)
+                    {
+                        for (int j = 0; j < intArray[i]; j++)
+                        {
+                            Console.Write(0);
+                        }
+                        position = Position.One;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < intArray[i]; j++)
+                        {
+                            Console.Write(1);
+                        }
+                        position = Position.Zero;
+                    }
+                }
+            }
+        }
+    }
+
+    enum Position
+    {
+        Zero,
+        One
     }
 }
