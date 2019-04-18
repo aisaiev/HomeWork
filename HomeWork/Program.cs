ff6c8a029716c36eed4ce73ce9d382ec3c652cd9
Anton <antonio.de.tores@gmail.com>
2019-04-18 22:50:26 +0300

    Task3 implemented

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -96,6 +96,25 @@ namespace HomeWork
             }
             Console.WriteLine($"Сумма к оплате: {sum}");
         }
+
+        private static void Task3()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
     }
 
     enum Position
