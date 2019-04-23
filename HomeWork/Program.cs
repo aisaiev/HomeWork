fd70f2cd275d313cf6481a8e5d8558c10f31a640
Anton <antonio.de.tores@gmail.com>
2019-04-23 05:47:22 +0300

    Task1 implemented

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -11,5 +11,18 @@ namespace HomeWork
         static void Main(string[] args)
         {
         }
+
+        private static void Task1()
+        {
+            string str = "How can mirrors be real if our eyes aren't real";
+            string[] words = str.Split(' ');
+            string[] resultWords = new string[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                resultWords[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+            }
+            string resultStr = string.Join(" ", resultWords);
+            Console.WriteLine(resultStr);
+        }
     }
 }
