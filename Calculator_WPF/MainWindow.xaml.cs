4e130d204d7bb2d7a41a9303b3d4d8eb3df08d07
Anton <antonio.de.tores@gmail.com>
2019-12-26 19:20:55 +0200

    Numeric buttons events implemented

--- Calculator_WPF/MainWindow.xaml.cs
+++ Calculator_WPF/MainWindow.xaml.cs
@@ -24,5 +24,61 @@ namespace Calculator_WPF
         {
             InitializeComponent();
         }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            switch (feSource.Name)
+            {
+                case "ButtonOne":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "1";
+                    break;
+                case "ButtonTwo":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "2";
+                    break;
+                case "ButtonThree":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "3";
+                    break;
+                case "ButtonFour":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "4";
+                    break;
+                case "ButtonFive":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "5";
+                    break;
+                case "ButtonSix":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "6";
+                    break;
+                case "ButtonSeven":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "7";
+                    break;
+                case "ButtonEight":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "8";
+                    break;
+                case "ButtonNine":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "9";
+                    break;
+                case "ButtonZero":
+                    IsInputTextBlockShouldBeCleared();
+                    this.InputTextBlock.Text += "0";
+                    break;
+            }
+        }
+
+        private void IsInputTextBlockShouldBeCleared()
+        {
+            if(this.InputTextBlock.Text.Length == 1 && this.InputTextBlock.Text == "0")
+            {
+                this.InputTextBlock.Text = string.Empty;
+            }
+        }
     }
 }
